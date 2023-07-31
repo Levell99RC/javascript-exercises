bdf9d3f6ff58d25488e84720f834a50157b5e256
Levell99RC <vincentsong96@gmail.com>
2023-07-31 13:40:30 -0400

    Filled in body of removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    for (let item of args) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === item) {
+                array.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
