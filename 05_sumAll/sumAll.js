e71a261673c676f1c741d420f65b1dc1aebb6ce1
Levell99RC <vincentsong96@gmail.com>
2023-07-31 14:00:44 -0400

    Filled body of sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(startInt, endInt) {
+    let sum = 0;
+    if (startInt > endInt) {
+        let temp = startInt;
+        startInt = endInt;
+        endInt = temp;
+    } else if (startInt < 0 || endInt < 0) {
+        return 'ERROR';
+    } else if ((typeof startInt) !== 'number' || (typeof endInt) !== 'number') {
+        return 'ERROR';
+    }
+    for (let a = startInt; a <= endInt; a++) {
+        sum += a;
+    }
+    return sum;
 };
 
 // Do not edit below this line
