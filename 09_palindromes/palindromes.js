35f8e2babbedacc8fd78ab9aa1ebe4e315d3e71e
Levell99RC <vincentsong96@gmail.com>
2023-08-02 22:32:48 -0400

    Add palindromes functionality

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    let chars = text.toLowerCase().split('');
+    console.log(chars);
+    let letters = chars.filter((item) => {
+        return item >= 'a' && item <= 'z' ||
+            item >= '0' && item <= '9';
+    });
+    console.log(letters);
+    for(let a = 0; a < Math.floor(letters.length / 2); a++) {
+        if(letters[a] !== letters[letters.length - 1 - a]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
