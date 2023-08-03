5e3e3e7cd2354545a32325a618ce0cc101b3626a
Levell99RC <vincentsong96@gmail.com>
2023-08-02 23:01:30 -0400

    Add body for fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(member) {
+    if(member < 0) {
+        return 'OOPS';
+    }
+    if(typeof member === 'string') {
+        member = parseInt(member);
+    }
+    let num1 = 1;
+    let num2 = 1;
+    for(let a = 3; a <= member; a++) {
+        let temp = num2;
+        num2 += num1;
+        num1 = temp;
+    }
+    return num2;
 };
 
 // Do not edit below this line
