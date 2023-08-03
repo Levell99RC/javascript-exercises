1c94a5fe4e64a707f8dca78e9d36b85eee8dc4ba
Levell99RC <vincentsong96@gmail.com>
2023-08-02 23:27:24 -0400

    Fill functionality for findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let sortedByAge = people.sort(function(a, b) {
+        let aTerm2;
+        let bTerm2;
+        if (a.yearOfDeath === undefined) {
+            aTerm2 = new Date().getFullYear();
+        } else {
+            aTerm2 = a.yearOfDeath;
+        }
+        if (b.yearOfDeath === undefined) {
+            bTerm2 = new Date().getFullYear();
+        } else {
+            bTerm2 = b.yearOfDeath;
+        }
+        return (aTerm2 - a.yearOfBirth) - (bTerm2 - b.yearOfBirth);
+    });
+    return sortedByAge[sortedByAge.length - 1];
 };
 
 // Do not edit below this line
